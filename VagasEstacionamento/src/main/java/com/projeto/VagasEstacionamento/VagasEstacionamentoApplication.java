a28f8f213573661f50926d9ed03d98bfc99a2e69
Ruan <133697229+RuanFioriMarcelino@users.noreply.github.com>
2026-06-09 20:27:01 -0300

    Add files via upload

---
+++ VagasEstacionamento/src/main/java/com/projeto/VagasEstacionamento/VagasEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.VagasEstacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasEstacionamentoApplication.class, args);
+	}
+
+}
